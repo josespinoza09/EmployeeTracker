b47fdf260cba64e0070e5371b3d23f0bf12631ad
Jose Espinoza <jose20019004@gmail.com>
2021-03-12 11:17:31 -0500

    added functions to view employees departments and roles

--- index.js
+++ index.js
@@ -63,3 +63,84 @@ async function questionsPrompt() {
 }
 questionsPrompt();
 
+async function viewAllEmployees() {
+  let allEmp = await db.query(
+    `SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.nameDepart AS department, roles.salary, CONCAT (manager.first_name, manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON manager.id = employee.manager_id;`
+  );
+  let empTable = await db.query("SELECT first_name FROM employee");
+
+  if (empTable.length === 0) {
+    console.log(`
+    ---------------------------
+    There are no employees yet!
+    ---------------------------
+    `);
+  } else {
+    console.table(allEmp);
+  }
+  questionsPrompt();
+}
+
+async function viewAllEmpDepartments() {
+  let departmentQuery = await db.query(`SELECT * FROM department`);
+  const departmentList = departmentQuery.map(({ id, nameDepart }) => ({
+    name: nameDepart,
+    value: id,
+  }));
+
+  if (departmentQuery.length === 0) {
+    console.log(`
+    ---------------------------
+    There are no employees yet!
+    ---------------------------
+    `);
+  } else {
+    const departmentAns = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Choose a department",
+        name: "department",
+        choices: departmentList,
+      },
+    ]);
+
+    let empDepartments = await db.query(
+      `SELECT employee.first_name, employee.last_name FROM employee AS employee LEFT JOIN roles AS roles ON employee.role_id = roles.id LEFT JOIN department AS department ON roles.department_id = department.id WHERE department.id = '${departmentAns.department}'`
+    );
+    console.table(empDepartments);
+  }
+  questionsPrompt();
+}
+
+async function viewAllEmpRoles() {
+  let rolesQuery = await db.query(`SELECT * FROM roles`);
+
+  const rolesList = rolesQuery.map(({ id, title }) => ({
+    name: title,
+    value: title,
+  }));
+
+  if (rolesQuery.length === 0) {
+    console.log(`
+    ---------------------------
+    There are no roles yet!
+    ---------------------------
+    `);
+  } else {
+    const rolesAns = await inquirer.prompt([
+      {
+        type: "list",
+        message: "Choose a role",
+        name: "role",
+        choices: rolesList,
+      },
+    ]);
+
+    let allRoles = await db.query(
+      `SELECT employee.first_name, employee.last_name FROM employee LEFT JOIN roles ON employee.role_id = roles.id WHERE roles.title = '${rolesAns.role}'`
+    );
+    console.table(allRoles);
+  }
+  questionsPrompt();
+}
+
